9fc39b8318831a204b9fb073a710f7e3b39f2d14
Felix Becker <felix.b@outlook.com>
2018-05-07 15:30:50 -0700

    Add repository management
    Get-SourcegraphRepositories
    Enable-SourcegraphRepository
    Disable-SourcegraphRepository

--- PSSourcegraph.psd1
+++ PSSourcegraph.psd1
@@ -12,7 +12,7 @@
     RootModule        = 'PSSourcegraph.psm1'
 
     # Version number of this module.
-    ModuleVersion     = '0.0.1' 
+    ModuleVersion     = '0.0.1'
 
     # Supported PSEditions
     # CompatiblePSEditions = @()
@@ -69,7 +69,7 @@
     # NestedModules = @()
 
     # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
-    FunctionsToExport = @('Invoke-SourcegraphApiRequest')
+    FunctionsToExport = @('Invoke-SourcegraphApiRequest', 'Get-SourcegraphRepositories', 'Enable-SourcegraphRepository', 'Disable-SourcegraphRepository')
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
     CmdletsToExport   = @()
@@ -78,7 +78,7 @@
     VariablesToExport = @()
 
     # Aliases to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no aliases to export.
-    AliasesToExport   = @('Invoke-SrcApiRequest')
+    AliasesToExport   = @('Invoke-SrcApiRequest', 'Get-SrcRepositories', 'Enable-SrcRepository', 'Disable-SrcRepository')
 
     # DSC resources to export from this module
     # DscResourcesToExport = @()
---
+++ src/repos.psm1
@@ -0,0 +1,87 @@
+
+$repositoriesQuery = Get-Content -Raw "$PSScriptRoot/queries/Repositories.graphql"
+
+function Get-SourcegraphRepositories {
+    <#
+    .SYNOPSIS
+        List all repositories known to a Sourcegraph instance
+    .DESCRIPTION
+        Lists all repositories known to a Sourcegraph instance by querying its API.
+    .PARAMETER Endpoint
+        The endpoint URL of the Sourcegraph instance (default https://sourcegraph.com)
+    .PARAMETER Token
+        The authentication token (if needed). Go to the settings page on Sourcegraph to generate one.
+    #>
+    [CmdletBinding(SupportsShouldProcess, SupportsPaging)]
+    param(
+        [string] $Endpoint = 'https://sourcegraph.com',
+
+        [ValidateNotNullOrEmpty()]
+        [string] $Token
+    )
+
+    $data = Invoke-SourcegraphApiRequest -Query $repositoriesQuery -Endpoint $Endpoint -Token $Token
+    $data.repositories.nodes
+    $PSCmdlet.PagingParameters.NewTotalCount($data.repositories.totalCount, 1)
+}
+Set-Alias Get-SrcRepositories Get-SourcegraphRepositories
+
+$setRepositoryEnabledQuery = Get-Content -Raw "$PSScriptRoot/queries/SetRepositoryEnabled.graphql"
+
+function Enable-SourcegraphRepository {
+    <#
+    .SYNOPSIS
+        Enables a repository on a Sourcegraph instance
+    .DESCRIPTION
+        Enables a repository on a Sourcegraph instance
+    .PARAMETER Id
+        The ID of the repository
+    .PARAMETER Endpoint
+        The endpoint URL of the Sourcegraph instance (default https://sourcegraph.com)
+    .PARAMETER Token
+        The authentication token (if needed). Go to the settings page on Sourcegraph to generate one.
+    #>
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Parameter(Mandatory, Position = 0, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [string] $Id,
+
+        [string] $Endpoint = 'https://sourcegraph.com',
+
+        [ValidateNotNullOrEmpty()]
+        [string] $Token
+    )
+
+    Invoke-SourcegraphApiRequest -Query $setRepositoryEnabledQuery -Variables @{repo = $Id; enabled = $true} -Endpoint $Endpoint -Token $Token | Out-Null
+}
+Set-Alias Enable-SrcRepository Enable-SourcegraphRepository
+
+function Disable-SourcegraphRepository {
+    <#
+    .SYNOPSIS
+        Disables a repository on a Sourcegraph instance
+    .DESCRIPTION
+        Disables a repository on a Sourcegraph instance
+    .PARAMETER Id
+        The ID of the repository
+    .PARAMETER Endpoint
+        The endpoint URL of the Sourcegraph instance (default https://sourcegraph.com)
+    .PARAMETER Token
+        The authentication token (if needed). Go to the settings page on Sourcegraph to generate one.
+    #>
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Parameter(Mandatory, Position = 0, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [string] $Id,
+
+        [string] $Endpoint = 'https://sourcegraph.com',
+
+        [ValidateNotNullOrEmpty()]
+        [string] $Token
+    )
+
+    Invoke-SourcegraphApiRequest -Query $setRepositoryEnabledQuery -Variables @{repo = $Id; enabled = $false} -Endpoint $Endpoint -Token $Token | Out-Null
+}
+Set-Alias Disable-SrcRepository Disable-SourcegraphRepository
