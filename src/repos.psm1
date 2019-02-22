49d2854cc7d23c4bc63608c6cf2546c011f2c4dd
Felix Becker <felix.b@outlook.com>
2019-02-22 22:21:39 +0100

    feat(repos): support more options for Get-Repository

--- src/repos.psm1
+++ src/repos.psm1
@@ -1,5 +1,6 @@
 
 $repositoryFields = Get-Content -Raw "$PSScriptRoot/queries/RepositoryFields.graphql"
+$repositoryQuery = (Get-Content -Raw "$PSScriptRoot/queries/Repository.graphql") + $repositoryFields
 $repositoriesQuery = (Get-Content -Raw "$PSScriptRoot/queries/Repositories.graphql") + $repositoryFields
 function Get-SourcegraphRepository {
     <#
@@ -7,24 +8,67 @@ function Get-SourcegraphRepository {
         List all repositories known to a Sourcegraph instance
     .DESCRIPTION
         Lists all repositories known to a Sourcegraph instance by querying its API.
+    .PARAMETER Id
+        Get a repository by its ID
+    .PARAMETER Name
+        Return repositories with the given names
+    .PARAMETER CloneUrl
+        Get a repository by a git clone URL
+    .PARAMETER Query
+        Return repositories whose names match the query
+    .PARAMETER SortBy
+        By what to sort
+    .PARAMETER Descending
+        Sort descending
     .PARAMETER Endpoint
         The endpoint URL of the Sourcegraph instance (default https://sourcegraph.com)
     .PARAMETER Token
         The authentication token (if needed). Go to the settings page on Sourcegraph to generate one.
     #>
-    [CmdletBinding(SupportsShouldProcess, SupportsPaging)]
+    [CmdletBinding(SupportsPaging)]
     param(
-        [string] $Endpoint = 'https://sourcegraph.com',
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [string] $Id,
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [string[]] $Name,
+        [string] $CloneUrl,
+        [string] $Query,
 
+        [ValidateSet('REPOSITORY_NAME', 'REPOSITORY_CREATED_AT')]
+        [string] $SortBy = 'REPOSITORY_NAME',
+        [switch] $Descending,
+
+        [string] $Endpoint = 'https://sourcegraph.com',
         [ValidateNotNullOrEmpty()]
         [string] $Token
     )
-
-    $data = Invoke-SourcegraphApiRequest -Query $repositoriesQuery -Endpoint $Endpoint -Token $Token
-    if ($PSCmdlet.PagingParameters.IncludeTotalCount) {
-        $PSCmdlet.PagingParameters.NewTotalCount($data.repositories.totalCount, 1)
+    process {
+        if ($Id -or $CloneUrl) {
+            $vars = @{
+                id       = $Id
+                cloneUrl = $CloneUrl
+            }
+            $data = Invoke-SourcegraphApiRequest -Query $repositoryQuery -Variables $vars -Endpoint $Endpoint -Token $Token
+            $data.repository
+            if ($PSCmdlet.PagingParameters.IncludeTotalCount) {
+                $count = if ($data.repository) { 1 } else { 0 }
+                $PSCmdlet.PagingParameters.NewTotalCount($count, 1)
+            }
+        } else {
+            $vars = @{
+                first      = $PSCmdlet.PagingParameters.Skip + $PSCmdlet.PagingParameters.First
+                query      = $Query
+                names      = $Name
+                orderBy    = $SortBy
+                descending = $Descending
+            }
+            $data = Invoke-SourcegraphApiRequest -Query $repositoriesQuery -Variables $vars -Endpoint $Endpoint -Token $Token
+            if ($PSCmdlet.PagingParameters.IncludeTotalCount) {
+                $PSCmdlet.PagingParameters.NewTotalCount($data.repositories.totalCount, 1)
+            }
+            $data.repositories.nodes | Select-Object -Skip $PSCmdlet.PagingParameters.Skip
+        }
     }
-    $data.repositories.nodes
 }
 Set-Alias Get-SrcRepositories Get-SourcegraphRepositories
 
@@ -55,7 +99,11 @@ function Enable-SourcegraphRepository {
         [string] $Token
     )
 
-    Invoke-SourcegraphApiRequest -Query $setRepositoryEnabledQuery -Variables @{repo = $Id; enabled = $true} -Endpoint $Endpoint -Token $Token | Out-Null
+    process {
+        if ($PSCmdlet.ShouldProcess("Enabling repository $Id", "Enable repository $Id?", "Enable repository")) {
+            Invoke-SourcegraphApiRequest -Query $setRepositoryEnabledQuery -Variables @{repo = $Id; enabled = $true} -Endpoint $Endpoint -Token $Token | Out-Null
+        }
+    }
 }
 Set-Alias Enable-SrcRepository Enable-SourcegraphRepository
 
@@ -84,6 +132,10 @@ function Disable-SourcegraphRepository {
         [string] $Token
     )
 
-    Invoke-SourcegraphApiRequest -Query $setRepositoryEnabledQuery -Variables @{repo = $Id; enabled = $false} -Endpoint $Endpoint -Token $Token | Out-Null
+    process {
+        if ($PSCmdlet.ShouldProcess("Disabling repository $Id", "Disable repository $Id?", "Disable repository")) {
+            Invoke-SourcegraphApiRequest -Query $setRepositoryEnabledQuery -Variables @{repo = $Id; enabled = $false} -Endpoint $Endpoint -Token $Token | Out-Null
+        }
+    }
 }
 Set-Alias Disable-SrcRepository Disable-SourcegraphRepository
